edda623b667fe9587ec89580b35250bca2db1bb9
IrynaSt <irinastoetski13@gmail.com>
2025-01-23 10:16:07 -0600

    week 9

---
+++ controllers/jobs.js
@@ -0,0 +1,35 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs });
+};
+
+const getJob = async (req, res) => {
+  res.send("getJob route");
+};
+
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
+const updateJob = async (req, res) => {
+  res.send("updateJob route");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("deleteJob route");
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  updateJob,
+  createJob,
+  deleteJob,
+};
